8b46431ab9b65a0cdbe04c56e9f7bc6fe67fff64
BastienCarbonnier <bastien.carbonnier@gmail.com>
2024-01-26 19:56:13 -0500

    update api

--- app/services/splinter-api.tsx
+++ app/services/splinter-api.tsx
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 export const mergeFiles = async (data: IJsonFile[]): Promise<any> => {
+  console.log(process.env.SPLINTER_API_URL);
   return await axios.post(`${process.env.SPLINTER_API_URL}/file`, { data }, {
     headers: {
       "Access-Control-Allow-Origin": "*",
