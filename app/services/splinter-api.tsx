4fbc640910b048ade8989834bab85682370a3f26
BastienCarbonnier <bastien.carbonnier@gmail.com>
2024-01-28 15:32:05 -0500

    Update env variable

--- app/services/splinter-api.tsx
+++ app/services/splinter-api.tsx
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 export const mergeFiles = async (data: IJsonFile[]): Promise<any> => {
-  console.log(process.env.SPLINTER_API_URL);
-  return await axios.post(`${process.env.SPLINTER_API_URL}/file`, { data }, {
+  console.log(process.env.NEXT_PUBLIC_SPLINTER_API_URL);
+  return await axios.post(`${process.env.NEXT_PUBLIC_SPLINTER_API_URL}/file`, { data }, {
     headers: {
       "Access-Control-Allow-Origin": "*",
       "Content-Type": "application/json",
