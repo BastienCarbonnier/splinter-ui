870ab9b6e0b602958d304abd03a7bdcd37bbb3d7
BastienCarbonnier <bastien.carbonnier@gmail.com>
2024-02-13 19:19:09 -0500

    main: Clean Page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,48 +1,12 @@
 "use client"
-import styles from './page.module.css'
-import { Provider } from 'react-redux';
-import { store } from './store';
-import Container from '@mui/material/Container';
-import { Grid, ThemeProvider, Typography, createTheme } from '@mui/material';
-import CssBaseline from '@mui/material/CssBaseline';
+import { createTheme } from '@mui/material';
 import React from 'react';
-import Header from './components/layout/header';
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
 import { redirect } from 'next/navigation';
 
-const darkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-  },
-});
-
 export default function Home() {
-  const { push } = useRouter();
   redirect('/merger');
   
   return (
-    <ThemeProvider theme={darkTheme}>
-      <CssBaseline />
-      <Provider store={store}>
-        <Header></Header>
-        <main className={styles.main}>
-          <Container>
-            <Grid container alignItems='center' marginTop='64px'>
-              <Grid item xs={12}>
-                <Typography variant="h3" marginBottom='1em' marginTop='1em'>
-                  Home
-                </Typography>
-              </Grid>
-              <Grid item xs={12}>
-                <Typography>
-                  Welcome to splinter a merger and validation tool for Json files.
-                </Typography>
-              </Grid>
-            </Grid>
-          </Container>
-        </main>
-      </Provider>
-    </ThemeProvider>
+    <></>
   )
 }
